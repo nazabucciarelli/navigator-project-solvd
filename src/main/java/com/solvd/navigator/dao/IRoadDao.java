d7f51fe4c3a28a34a2af087faaef94591de7dba4
Atilio <atilioboher@gmail.com>
2024-01-20 08:33:07 -0300

    Add java docs to interfaces

--- src/main/java/com/solvd/navigator/dao/IRoadDao.java
+++ src/main/java/com/solvd/navigator/dao/IRoadDao.java
@@ -6,7 +6,19 @@ import java.util.List;
 
 public interface IRoadDao extends IBaseDao<Road> {
 
+    /**
+     * Returns the road with the matching id. If there is not a match,
+     * a road with id=0 is returned.
+     *
+     * @param id of the road
+     * @return Road object
+     */
     Road getById(int id);
 
+    /**
+     * Returns a list with all the entries from the roads table
+     *
+     * @return List of Road
+     */
     List<Road> getAll();
 }
