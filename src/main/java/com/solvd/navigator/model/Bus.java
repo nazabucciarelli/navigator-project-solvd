1146e5f7bf09059ccbf88e9d3815afb7017c1ccf
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-01-27 14:58:51 -0300

    add entity, dao and service for buses

---
+++ src/main/java/com/solvd/navigator/model/Bus.java
@@ -0,0 +1,51 @@
+package com.solvd.navigator.model;
+
+import java.util.List;
+
+public class Bus {
+    private int id;
+    private String name;
+    private List<Station> stations;
+
+    public Bus() {
+    }
+
+    public Bus(int id, String name, List<Station> stations) {
+        this.id = id;
+        this.name = name;
+        this.stations = stations;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<Station> stations) {
+        this.stations = stations;
+    }
+
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", stations=" + stations +
+                '}';
+    }
+}
